8472e26b3ecbc033601b7697f32127c402241be4
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-04-20 08:55:42 +0530

    feat: mobile nav expanded state

--- src/components/Navigation/Nav.tsx
+++ src/components/Navigation/Nav.tsx
@@ -1,4 +1,7 @@
+import { NavLink } from 'react-router-dom'
+
 import { ReactComponent as SearchLogo } from '../../assets/svg/ic_round-search.svg'
+import { ReactComponent as ExternalLinkIcon } from '../../assets/svg/icon_external-link.svg'
 import { ReactComponent as Logo } from '../../assets/svg/invento-logo-red.svg'
 import { ReactComponent as Menu } from '../../assets/svg/menu.svg'
 import { useToggle } from '../../hooks'
@@ -21,6 +24,44 @@ export default function Nav() {
                 <span className="sr-only">Menu</span>
                 <Menu className="menu" />
             </button>
+            <nav
+                className="main-nav grid bg-black ff-serif fw-300 text-white"
+                data-expanded={navState}
+            >
+                <ul className="primary-navigation flow" id="primary-navigation">
+                    <li>
+                        <NavLink className={`navlink flex`} to={'/saptha'}>
+                            tickets <ExternalLinkIcon />
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={`navlink flex`} to={'/saptha'}>
+                            home <ExternalLinkIcon />
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={`navlink flex`} to={'/saptha'}>
+                            events <ExternalLinkIcon />
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={`navlink flex`} to={'/saptha'}>
+                            saptha <ExternalLinkIcon />
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={`navlink flex`} to={'/saptha'}>
+                            link <ExternalLinkIcon />
+                        </NavLink>
+                    </li>
+                </ul>
+                <div className="nav--footer">
+                    <p className="fw-400 fs-800">
+                        invento
+                        <span className="d-b fs-150">gec palakad</span>
+                    </p>
+                </div>
+            </nav>
         </header>
     )
 }
