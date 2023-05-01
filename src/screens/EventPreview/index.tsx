d32f5158fc60fd26dbf044d000f4d5abd1eaf966
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-05-01 12:12:05 +0530

    feat: event/preview

--- src/screens/EventPreview/index.tsx
+++ src/screens/EventPreview/index.tsx
@@ -1,35 +1,70 @@
 import { NavLink } from 'react-router-dom'
 
 import Nathya from '../../assets/images/compressed/natya_large.jpg'
-import Card from '../../components/Card'
 
 export default function EventPreview() {
     return (
         <section className="eventPreview">
-            <Card className="card__lg" title="Natya" bgUrl={Nathya} />
-            <div
-                className="eventPreview__about text-white ff-serif
-            "
-            >
-                <h4 className="fw-500">About Natya</h4>
-                <p className="eventPreview__description">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias minima
-                    reiciendis vel quo dolore dolor in laboriosam et, repellendus
-                    exercitationem quos quis fugit. Sunt error delectus odit libero
-                    perferendis tenetur! Unde ratione, possimus nobis maiores quia error
-                    illum sint sunt adipisci quos autem, nisi iste quibusdam, in
-                    provident. Voluptates voluptatem, quaerat temporibus nobis inventore
-                    labore earum incidunt fugiat sit aspernatur?
+            <div className="bg__container grid">
+                <div className="bg--full bg--full--preview"></div>
+
+                <p className="eventPreview__title ff-days-one fw-400 text-white uppercase ">
+                    Natya
                 </p>
-                <div>
-                    <span className="d-b">Date: 22-06-2023</span>
-                    <span className="d-b">Prizes worth: 10k</span>
-                </div>
-                <NavLink to={'/register'} className={`btn btn--link btn--register_event`}>
-                    register now
-                </NavLink>
             </div>
-            <div className="eventPreview__rules text-white ff-serif"></div>
+            <div className="eventPreview__content panel--bg mh-full grid">
+                <div className="eventPreview__about text-white ff-serif centeredContainer side-padding flow">
+                    <h4 className="fw-500 fs-650">About Natya</h4>
+                    <p className="eventPreview__description">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
+                        minima reiciendis vel quo dolore dolor in laboriosam et,
+                        repellendus exercitationem quos quis fugit. Sunt error delectus
+                        odit libero perferendis tenetur! Unde ratione, possimus nobis
+                        maiores quia error illum sint sunt adipisci quos autem, nisi iste
+                        quibusdam, in provident. Voluptates voluptatem, quaerat temporibus
+                        nobis inventore labore earum incidunt fugiat sit aspernatur?
+                    </p>
+                    <div className="flow">
+                        <span className="d-b">Date: 22-06-2023</span>
+                        <span className="d-b">Prizes worth: 10k</span>
+                        <NavLink
+                            to={'/register'}
+                            className={`text-magenta btn d-ib btn--link btn--register_event`}
+                        >
+                            register now
+                        </NavLink>
+                    </div>
+                </div>
+                <div className="eventPreview__rules text-white ff-serif centeredContainer side-padding">
+                    <h4 className="fs-650 fw-500">Rules and Regulations</h4>
+                    <ul className="flow">
+                        <li>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            Beatae, repudiandae!
+                        </li>
+                        <li>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            Beatae, repudiandae!
+                        </li>
+                        <li>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            Beatae, repudiandae!
+                        </li>
+                        <li>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            Beatae, repudiandae!
+                        </li>
+                        <li>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            Beatae, repudiandae!
+                        </li>
+                        <li>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            Beatae, repudiandae!
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </section>
     )
 }
