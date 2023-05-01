ad7d3d24c0a79366daee7663bb81ca5e907d78bc
cgaswin <cgaswin333@gmail.com>
2023-05-01 16:32:06 +0530

    added campusAmbassadors collection

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,4 +1,6 @@
 import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -12,3 +14,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
+export const storage = getStorage(app)
