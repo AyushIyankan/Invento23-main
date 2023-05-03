0bb1822c7baf7aac20d8c0a2dbc0498383c6d9f8
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-05-03 11:06:05 +0530

    feat: forminput component

--- src/components/Form/FormInput.tsx
+++ src/components/Form/FormInput.tsx
@@ -1,60 +1,67 @@
+import { ErrorMessage } from '@hookform/error-message'
 import { HTMLInputTypeAttribute } from 'react'
 import { FieldErrors, FieldValues, Path, UseFormRegister } from 'react-hook-form'
 
 export type IFormInput<T extends FieldValues> = {
     label: Path<T>
-    inputType?: HTMLInputTypeAttribute
     register: UseFormRegister<T>
     errors?: FieldErrors<T>
     required?: boolean
     forEl: Path<T>
-    options?: readonly ['1', '2', '3', '4']
-} & React.HTMLProps<HTMLInputElement>
+} & (
+    | {
+          kind: 'input'
+          inputType: HTMLInputTypeAttribute
+      }
+    | {
+          kind: 'select'
+          options: readonly ['1', '2', '3', '4']
+      }
+)
 
 export function FormInput<T extends FieldValues>({
     label,
-    inputType,
     register,
     errors,
     required,
     forEl,
-    options,
+    ...props
 }: IFormInput<T>) {
     const { ref, ...rest } = register(forEl, { required })
-    const { message: errorMessage } = errors?.[`${label}`] || {}
 
-    const errorElement = (
-        <span className="FormInputErrorMessage">{errorMessage as string}</span>
-    )
+    // console.log(`errors: `, errors?.[`${label}`])
 
-    const selectInput = (
-        <select {...rest} name={forEl} id={forEl} ref={(node) => ref(node)}>
-            <option value="">Year</option>
-            {options?.map((opt) => (
-                <option value={opt} key={`index-${opt}`} label={opt}>
-                    opt
-                </option>
-            ))}
-        </select>
-    )
-
-    const textInput = (
-        <input
-            {...rest}
-            name={forEl}
-            type={`${inputType}`}
-            id={`${forEl}`}
-            ref={(node) => {
-                ref(node)
-            }}
-        />
-    )
-
-    return (
-        <div className="FormInputWrap">
-            <label htmlFor={`${forEl}`}>{label}</label>
-            {options ? selectInput : textInput}
-            {errorMessage && errorElement}
-        </div>
-    )
+    switch (props.kind) {
+        case 'input':
+            return (
+                <div className="FormInputWrap">
+                    <label htmlFor={`${forEl}`}>{label}</label>
+                    <input
+                        {...rest}
+                        name={forEl}
+                        type={props.inputType}
+                        id={`${forEl}`}
+                        ref={(node) => {
+                            ref(node)
+                        }}
+                    />
+                    <span className="FormInpuError">{errors?.[`${forEl}`]?.message}</span>
+                </div>
+            )
+        case 'select':
+            return (
+                <div className="FormInputWrap">
+                    <label htmlFor={forEl}>{label}</label>
+                    <select {...rest} name={forEl} id={forEl} ref={(node) => ref(node)}>
+                        <option value="">Year</option>
+                        {props.options?.map((opt) => (
+                            <option value={opt} key={`index-${opt}`} label={opt}>
+                                opt
+                            </option>
+                        ))}
+                    </select>
+                    {/* <ErrorMessage name={forEl} errors={errors?.[`${label}`]} /> */}
+                </div>
+            )
+    }
 }
