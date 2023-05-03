fbf62df7221c024e7799bc23e17d4570ec203353
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-05-03 11:06:25 +0530

    feat: form

--- src/screens/Register/Form.tsx
+++ src/screens/Register/Form.tsx
@@ -2,7 +2,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 
 import { FormInput } from '../../components/Form'
-import { Fields, FormSchema, formSchema } from './schema'
+import { Fields, formSchema } from './schema'
 
 interface FormInputs {
     name: string
@@ -17,37 +17,55 @@ export function RegistrationForm() {
     const {
         register,
         handleSubmit,
-        formState: { errors },
+        formState: { errors, isValid },
     } = useForm<FormInputs>({
         mode: 'onBlur',
         resolver: zodResolver(formSchema),
     })
 
     const InputFields = Fields.map((field) => {
-        return (
-            <FormInput
-                key={field.id}
-                label={field.name as keyof FormInputs}
-                forEl={field.for as keyof FormInputs}
-                register={register}
-                errors={errors}
-                required={field.required}
-                inputType={field.type}
-                options={field.options || undefined}
-            />
-        )
+        switch (field.kind) {
+            case 'input':
+                return (
+                    <FormInput
+                        key={field.id}
+                        label={field.name as keyof FormInputs}
+                        forEl={field.for as keyof FormInputs}
+                        register={register}
+                        errors={errors}
+                        required={field.required}
+                        kind={field.kind}
+                        inputType={field.type}
+                    />
+                )
+            case 'select':
+                return (
+                    <FormInput
+                        key={field.id}
+                        label={field.name as keyof FormInputs}
+                        forEl={field.for as keyof FormInputs}
+                        register={register}
+                        errors={errors}
+                        required={field.required}
+                        kind={field.kind}
+                        options={field.options}
+                    />
+                )
+        }
     })
 
-    const onSubmit = (data: any) => {
+    const onSubmit = (data: FormInputs) => {
         console.log(data)
     }
-
+    console.log(isValid)
+    console.log(errors)
     return (
-        <div className="centeredContainer">
+        <div className="">
             <h2 className="text-black fw-400 ff-serif ">Personal Information</h2>
             <div className="FormWrap">
                 <form onSubmit={handleSubmit(onSubmit)} className="Form">
                     {InputFields}
+                    <button type="submit">Submit</button>
                 </form>
             </div>
         </div>
