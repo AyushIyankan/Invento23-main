80ca2ad273949bdcc233aea7260c72f9b94311a0
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-05-11 21:54:42 +0530

    migrate to react 18

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import './scss/main.scss'
 
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from './App'
 
-ReactDOM.render(
+const container = document.getElementById('root')
+
+const root = createRoot(container!)
+
+root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
-    document.getElementById('root'),
 )
