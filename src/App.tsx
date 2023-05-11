2693b7f934875af4928d87311d1f65a8a4490e0b
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-05-11 07:39:05 +0530

    feat: code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,128 @@
+import { lazy, Suspense } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import { AboutLayout, AboutUs } from './pages/AboutUs'
-import Events, { Layout as EventsLayout } from './pages/events'
-import { Leaderboard, LeaderboardLayout } from './pages/Leaderboard'
-import Saptha, { Layout as SapthaLayout } from './pages/saptha'
-import EventPreview from './screens/EventPreview'
-import { NotFound } from './screens/NotFound'
-import Register from './screens/Register'
-import { FormLayout } from './screens/Register'
+const LazyNotFound = lazy(() =>
+    import('./screens/NotFound').then((m) => {
+        return { default: m.NotFound }
+    }),
+)
+
+const LazyLeaderBoardLayout = lazy(() =>
+    import('./pages/Leaderboard/Layout').then((m) => {
+        return { default: m.Layout }
+    }),
+)
+
+const LazyLeaderBoard = lazy(() =>
+    import('./pages/Leaderboard/Leaderboard').then((m) => {
+        return { default: m.Leaderboard }
+    }),
+)
+
+const LazyAboutLayout = lazy(() =>
+    import('./pages/AboutUs/Layout').then((m) => {
+        return { default: m.Layout }
+    }),
+)
+
+const LazyAbout = lazy(() =>
+    import('./pages/AboutUs').then((m) => {
+        return { default: m.AboutUs }
+    }),
+)
+
+const SapthaLazyLayout = lazy(() =>
+    import('./pages/saptha/Layout').then((m) => {
+        return { default: m.Layout }
+    }),
+)
+const SapthaLazy = lazy(() => import('./pages/saptha'))
+
+const EventsLazyLayout = lazy(() =>
+    import('./pages/events/Layout').then((m) => {
+        return { default: m.Layout }
+    }),
+)
+
+const EventsLazy = lazy(() => import('./pages/events/Events'))
+const EventsPreviewLazy = lazy(() => import('./screens/EventPreview'))
+
+const LazyFormLayout = lazy(() =>
+    import('./screens/Register/Layout').then((m) => {
+        return { default: m.FormLayout }
+    }),
+)
+
+const LazyRegister = lazy(() => import('./screens/Register'))
 
 const routes = createBrowserRouter([
     {
         path: '*',
-        element: <NotFound />,
+        element: <LazyNotFound />,
     },
     {
         path: '/saptha',
-        element: <SapthaLayout />,
+        element: <SapthaLazyLayout />,
         children: [
             {
                 index: true,
-                element: <Saptha />,
+                element: <SapthaLazy />,
             },
         ],
     },
 
     {
         path: '/events',
-        element: <EventsLayout />,
+        element: <EventsLazyLayout />,
         children: [
             {
                 index: true,
-                element: <Events />,
+                element: <EventsLazy />,
             },
             {
                 path: '/events/:id',
-                element: <EventPreview />,
+                element: <EventsPreviewLazy />,
             },
         ],
     },
     {
         path: '/register',
-        element: <FormLayout />,
+        element: <LazyFormLayout />,
         children: [
             {
                 index: true,
-                element: <Register />,
+                element: <LazyRegister />,
             },
         ],
     },
     {
         path: '/about',
-        element: <AboutLayout />,
+        element: <LazyAboutLayout />,
         children: [
             {
                 index: true,
-                element: <AboutUs />,
+                element: <LazyAbout />,
             },
         ],
     },
     {
         path: '/leaderboards',
-        element: <LeaderboardLayout />,
+        element: <LazyLeaderBoardLayout />,
         children: [
             {
                 index: true,
-                element: <Leaderboard />,
+                element: <LazyLeaderBoard />,
             },
         ],
     },
 ])
 
 function App() {
-    return <RouterProvider router={routes} />
+    return (
+        <Suspense fallback={<h1>Loading...</h1>}>
+            <RouterProvider router={routes} />
+        </Suspense>
+    )
 }
 
 export default App
