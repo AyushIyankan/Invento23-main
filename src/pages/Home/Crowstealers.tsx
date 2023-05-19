b960406853fbfd632473bdf02de1f07bd3c52bac
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-05-20 03:26:47 +0530

    add colors to proshow cards

--- src/pages/Home/Crowstealers.tsx
+++ src/pages/Home/Crowstealers.tsx
@@ -9,16 +9,19 @@ export default function Crowstealers() {
                     date="June 24"
                     imgSrc="/static/landing/stock-ev.jpg"
                     title="the avial band"
+                    color="hsl(56, 100%, 78%)"
                 />
                 <EventCard
                     date="June 24"
                     imgSrc="/static/landing/stock-ev.jpg"
                     title="benny dayal"
+                    color="hsl(313, 88%, 53%)"
                 />
                 <EventCard
                     date="June 24"
                     imgSrc="/static/landing/stock-ev.jpg"
                     title="the nandi sisters"
+                    color="hsl(186, 71%, 46%)"
                 />
             </div>
         </div>
