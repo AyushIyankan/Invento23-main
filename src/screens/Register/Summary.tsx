f47bfd476334a31d9e86276da2c3175fcbb1f775
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-05-30 12:21:03 +0530

    move to framer for animations

--- src/screens/Register/Summary.tsx
+++ src/screens/Register/Summary.tsx
@@ -1,5 +1,5 @@
+import { AnimatePresence, motion } from 'framer-motion'
 import { useState } from 'react'
-import { animated, useTransition } from 'react-spring'
 
 import Button from '../../components/Button'
 import { ItemCard } from '../../components/Card'
@@ -8,52 +8,71 @@ export function Summary() {
     const { items: bucket, removeItem } = useStore((state) => state)
     const [selectedindex, setSelectedindex] = useState(0)
 
-    const transitions = useTransition(bucket, {
-        from: { opacity: 0.3, y: '-100%' },
-        enter: { opacity: 1, y: '0%' },
-        leave: { opacity: 0, y: '100%' },
-    })
-
-    const bucketItems = transitions((style, item) => (
-        <animated.div style={style}>
+    const bucketItems = bucket.map((item) => (
+        <motion.div
+            layout
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ opacity: 0, scale: 0.8 }}
+            key={`wrapper-` + item.id}
+            transition={{ type: 'spring' }}
+        >
             <ItemCard
                 key={item?.id}
-                title="Item"
-                date="22/03/22"
-                fee={200}
-                image={item?.image}
+                title={item.title}
+                date={item.date}
+                fee={Number(item.fee)}
+                image={item.image}
                 actionType="nonTogglable"
                 action={() => removeItem(item.id)}
                 selected={true}
-                onClick={() => setSelectedindex(Number(item.id))}
+                onClick={() => {
+                    setSelectedindex(Number(item.id))
+                    // setisFilled((state) => !state)
+                }}
             />
-        </animated.div>
+        </motion.div>
     ))
-
     return (
         <>
             <h3 className="text-black ff-serif fw-400">Booking Summary</h3>
             <div className="form__eventsWrap form__summaryWrap grid">
-                <div
-                    className="form__eventsSummary grid"
-                    data-filled={bucket.length && true}
-                >
-                    {bucketItems}
-                </div>
-                {bucket.length === 0 ? (
-                    <p className="bucket-no-item-fallback t-center ff-serif text-grey capitalize">
-                        {' '}
-                        select some events{' '}
-                    </p>
-                ) : (
-                    <Button
-                        type="internalUrl"
-                        to="/checkout"
-                        classNames="btn btn--link btn--save text-white ff-serif btn--checkout"
-                    >
-                        Checkout
-                    </Button>
-                )}
+                <AnimatePresence>
+                    {bucket.length ? (
+                        <motion.div
+                            className="grid"
+                            // exit={{ opacity: 0, scale: 0.8 }}
+                            // initial={{ opacity: 0, scale: 0.8 }}
+                            // animate={{ opacity: 1, scale: 1 }}
+                        >
+                            <div
+                                className="form__eventsSummary grid"
+                                data-filled={bucket.length && true}
+                            >
+                                <AnimatePresence mode="popLayout">
+                                    {bucketItems}
+                                </AnimatePresence>
+                            </div>
+                            <Button
+                                type="internalUrl"
+                                to="/checkout"
+                                classNames="btn btn--link btn--save text-white ff-serif btn--checkout"
+                            >
+                                Checkout
+                            </Button>
+                        </motion.div>
+                    ) : (
+                        <motion.p
+                            className="bucket-no-item-fallback t-center ff-serif text-grey capitalize"
+                            exit={{ opacity: 0 }}
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                        >
+                            {' '}
+                            select some events{' '}
+                        </motion.p>
+                    )}
+                </AnimatePresence>
             </div>
         </>
     )
