6c8ebaaaaea787fb20f5e0de14dcdee36ee8311d
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-08-12 09:08:31 +0530

    feat: add pulsing effect to the image while loading

--- src/pages/Home/data.ts
+++ src/pages/Home/data.ts
@@ -5,30 +5,36 @@ export const SLIDERITEMS: SliderItemProps[] = [
         id: '1',
         title: 'Slide 1',
         src: '/static/landing/image-stack-top-2.jpg',
+        // src: 'https://source.unsplash.com/random/',
     },
     {
         id: '2',
         title: 'Slide 2',
         src: '/static/landing/image-stack-top-3.jpg',
+        // src: 'https://source.unsplash.com/random/',
     },
     {
         id: '3',
         title: 'Slide 3',
         src: '/static/landing/image-stack-top-4.jpg',
+        // src: 'https://source.unsplash.com/random/',
     },
     {
         id: '4',
         title: 'Slide 4',
         src: '/static/landing/image-stack-top-5.jpg',
+        // src: 'https://source.unsplash.com/random/',
     },
     {
         id: '5',
         title: 'Slide 5',
         src: '/static/landing/image-stack-top-6.jpg',
+        // src: 'https://source.unsplash.com/random/',
     },
     {
         id: '6',
         title: 'Slide 6',
         src: '/static/landing/image-stack-top-7.jpg',
+        // src: 'https://source.unsplash.com/random/',
     },
 ]
