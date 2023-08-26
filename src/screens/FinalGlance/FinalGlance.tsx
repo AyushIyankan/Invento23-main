6549a3d7da468254e4cdb025d5fbe92efafe6c70
Mohammed Hashique <73403500+hashiq300@users.noreply.github.com>
2023-08-26 10:58:33 +0530

    Layout fix (#48)
    * fix: changed react-router to react-router-dom
    
    * fix: layout shift due to position absolute

--- src/screens/FinalGlance/FinalGlance.tsx
+++ src/screens/FinalGlance/FinalGlance.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router'
+import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 
 import Button from '../../components/Button'
